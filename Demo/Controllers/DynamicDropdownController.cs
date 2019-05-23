f9718c2464e2c5f8b8ebc2ad53ca6db17b360b9e
Faisal Pathan <faisalmpathan@gmail.com>
2019-05-23 17:05:29 +0530

    Initial Commit

---
+++ Demo/Controllers/DynamicDropdownController.cs
@@ -0,0 +1,50 @@
+﻿using Demo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Demo.Controllers
+{
+    public class DynamicDropdownController : Controller
+    {
+        // GET: DynamicDropdown
+        public ActionResult Index()
+        {
+            //Using Viewbag
+            List<SelectListItem> drpList = new List<SelectListItem>();
+            drpList.Add(new SelectListItem { Text = "Faisal", Value = "1" });
+            drpList.Add(new SelectListItem { Text = "Irshad", Value = "1" });
+            drpList.Add(new SelectListItem { Text = "Kaushik", Value = "1" });
+            drpList.Add(new SelectListItem { Text = "Jaimin", Value = "1" });
+            drpList.Add(new SelectListItem { Text = "Kirti", Value = "1" });
+            ViewBag.DrpListViewBag = drpList;
+
+            //Using ViewData
+            List<SelectListItem> drpListViewData = new List<SelectListItem>();
+            drpListViewData.Add(new SelectListItem { Text = "Faisal", Value = "1" });
+            drpListViewData.Add(new SelectListItem { Text = "Irshad", Value = "1" });
+            drpListViewData.Add(new SelectListItem { Text = "Kaushik", Value = "1" });
+            drpListViewData.Add(new SelectListItem { Text = "Jaimin", Value = "1" });
+            drpListViewData.Add(new SelectListItem { Text = "Kirti", Value = "1" });
+            ViewData["Names"] = drpListViewData;
+
+            //Using TempData
+            List<SelectListItem> drpListTempData = new List<SelectListItem>();
+            drpListTempData.Add(new SelectListItem { Text = "Faisal", Value = "1" });
+            drpListTempData.Add(new SelectListItem { Text = "Irshad", Value = "1" });
+            drpListTempData.Add(new SelectListItem { Text = "Kaushik", Value = "1" });
+            drpListTempData.Add(new SelectListItem { Text = "Jaimin", Value = "1" });
+            drpListTempData.Add(new SelectListItem { Text = "Kirti", Value = "1" });
+            TempData["Names"] = drpListTempData;
+            return View();
+        }
+        public ActionResult GetListData()
+        {
+            Context _context = new Context();
+            var data = _context.Angulars.ToList();
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ Demo/Models/Context.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Demo.Models
+{
+    public class Context : DbContext
+    {
+        public Context() : base("StringDBContext") { }
+        public DbSet<Angular> Angulars { get; set; }
+    }
+}
